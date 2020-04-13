50ba9c48f03fb6d764ebf2df0012ca90b0aa95d2
Prudence Gaboriau <prudence.gaboriau@ext.vallourec.com>
2020-04-13 19:16:33 +0200

    feat(master): install orm/doctine and create first controller which return an html view

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController {
+  public function index() {
+    return new Response('<p>Hello !</p>');
+  }
+}
